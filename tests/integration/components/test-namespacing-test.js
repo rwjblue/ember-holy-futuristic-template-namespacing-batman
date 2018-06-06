00a3506c22085bb6ad64494fac62286701b0d424
Robert Jackson <rjackson@linkedin.com>
2018-06-06 18:38:30 -0400

    Run ember-cli-update codemods. <3

--- tests/integration/components/test-namespacing-test.js
+++ tests/integration/components/test-namespacing-test.js
@@ -1,6 +1,10 @@
 /* globals define */
-import Ember from 'ember';
-import { moduleForComponent, test } from 'ember-qunit';
+import { helper as buildHelper } from '@ember/component/helper';
+
+import Component from '@ember/component';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 define('other-namespace/templates/components/some-template-thing', ['exports'], function(exports) {
@@ -8,36 +12,36 @@ define('other-namespace/templates/components/some-template-thing', ['exports'], 
 });
 
 define('other-namespace/components/some-js-thing', ['exports'], function(exports) {
-  exports.default = Ember.Component.extend({
+  exports.default = Component.extend({
     tagName: 'p',
     classNames: ['some-js-thing']
   });
 });
 
 define('other-namespace/helpers/some-helper-thing', ['exports'], function(exports) {
-  exports.default = Ember.Helper.helper(function([arg1]) {
+  exports.default = buildHelper(function([arg1]) {
     return arg1;
   });
 });
 
-moduleForComponent('test-namespacing', {
-  integration: true,
-});
+module('test-namespacing', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it can render a helper', function(assert) {
-  this.render(hbs`{{other-namespace::some-helper-thing 'hi'}}`);
+  test('it can render a helper', async function(assert) {
+    await render(hbs`{{other-namespace::some-helper-thing 'hi'}}`);
 
-  assert.equal(this.$().text().trim(), 'hi');
-});
+    assert.equal(find('*').textContent.trim(), 'hi');
+  });
 
-test('it can render a template only component', function(assert) {
-  this.render(hbs`{{other-namespace::some-template-thing derp="here"}}`);
+  test('it can render a template only component', async function(assert) {
+    await render(hbs`{{other-namespace::some-template-thing derp="here"}}`);
 
-  assert.equal(this.$().text().trim(), 'some-template-thing');
-});
+    assert.equal(find('*').textContent.trim(), 'some-template-thing');
+  });
 
-test('it can render a JS only component', function(assert) {
-  this.render(hbs`{{#other-namespace::some-js-thing}}hi{{/other-namespace::some-js-thing}}`);
+  test('it can render a JS only component', async function(assert) {
+    await render(hbs`{{#other-namespace::some-js-thing}}hi{{/other-namespace::some-js-thing}}`);
 
-  assert.equal(this.$().text().trim(), 'hi');
+    assert.equal(find('*').textContent.trim(), 'hi');
+  });
 });
