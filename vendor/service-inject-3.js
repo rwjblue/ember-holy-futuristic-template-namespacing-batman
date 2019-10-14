96d371c9845706ee296492bc6ce85bbe53c9e014
Seena Rowhani <seenarowhani95@gmail.com>
2019-10-14 16:41:01 -0700

    fix(service): propagate original args into original inject fn

--- vendor/service-inject-3.js
+++ vendor/service-inject-3.js
@@ -1,13 +1,21 @@
 /* globals Ember */
 /* eslint-disable ember/new-module-imports */
 
+const { isElementDescriptor } = Ember.__loader.require('@ember/-internals/metal');
+
 (function() {
-  var ORIGINAL_INJECT_SERVICE = Ember.inject.service;
-  Ember.inject.service = function(name) {
-    if (name === undefined) {
-      return ORIGINAL_INJECT_SERVICE.call(this, name);
+  const ORIGINAL_INJECT_SERVICE = Ember.inject.service;
+  Ember.inject.service = function(...args) {
+    const resolvedName = isElementDescriptor(args) ? undefined : args[0];
+
+    if (resolvedName === undefined) {
+      return ORIGINAL_INJECT_SERVICE.call(this, ...args);
     }
 
-    return ORIGINAL_INJECT_SERVICE.call(this, name.replace('$', '@'));
+    const mutatedName = resolvedName.replace('$', '@');
+    const mutatedArgs = args.slice();
+    mutatedArgs.splice(mutatedArgs.indexOf(resolvedName), 1, mutatedName);
+
+    return ORIGINAL_INJECT_SERVICE.call(this, ...mutatedArgs);
   };
 })();
