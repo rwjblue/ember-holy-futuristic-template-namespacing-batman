e73a1a919c3d75194d146a1523a0315d68ca3a8d
Seena Rowhani <seenarowhani95@gmail.com>
2019-12-11 17:52:15 -0800

    fix(service): tryInvoke getElementDescriptor

--- tests/unit/services/namespaced-services-test.js
+++ tests/unit/services/namespaced-services-test.js
@@ -2,6 +2,7 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import { default as Service, inject } from '@ember/service';
+import hasEmberVersion from 'ember-test-helpers/has-ember-version';
 
 define('other-namespace/services/some-thing', ['exports'], function(exports) {
   exports.default = Service.extend({
@@ -22,13 +23,15 @@ module('Unit | Service | namespaced-services', function(hooks) {
     assert.equal(service.get('someService').sayHi(), 'hi!');
   });
 
-  test('doesn\'t break services injected using decorators', function (assert) {
-    this.owner.register('service:subject', class Subject extends Service {
-      @inject('other-namespace$some-thing') bar;
+  if (hasEmberVersion(3, 10)) {
+    test('doesn\'t break services injected using decorators', function (assert) {
+      this.owner.register('service:subject', class Subject extends Service {
+        @inject('other-namespace$some-thing') bar;
+      });
+
+      const service = this.owner.lookup('service:subject');
+
+      assert.equal(service.get('bar').sayHi(), 'hi!');
     });
-
-    let service = this.owner.lookup('service:subject');
-
-    assert.equal(service.get('bar').sayHi(), 'hi!');
-  })
+  }
 });
--- vendor/service-inject-3.js
+++ vendor/service-inject-3.js
@@ -1,21 +1,23 @@
 /* globals Ember */
 /* eslint-disable ember/new-module-imports */
 
-const { isElementDescriptor } = Ember.__loader.require('@ember/-internals/metal');
-
 (function() {
+  const { require: emberRequire } = Ember.__loader;
+
+  function maybeRequire(moduleName) {
+    return emberRequire.has(moduleName) && emberRequire(moduleName);
+  }
+
+  const emberMetal = maybeRequire('@ember/-internals/metal');
+
   const ORIGINAL_INJECT_SERVICE = Ember.inject.service;
   Ember.inject.service = function(...args) {
-    const resolvedName = isElementDescriptor(args) ? undefined : args[0];
-
-    if (resolvedName === undefined) {
+    if (Ember.tryInvoke(emberMetal, 'isElementDescriptor', args)) {
       return ORIGINAL_INJECT_SERVICE.call(this, ...args);
     }
 
-    const mutatedName = resolvedName.replace('$', '@');
-    const mutatedArgs = args.slice();
-    mutatedArgs.splice(mutatedArgs.indexOf(resolvedName), 1, mutatedName);
-
-    return ORIGINAL_INJECT_SERVICE.call(this, ...mutatedArgs);
+    const mutatedName = args[0].replace('$', '@');
+    args.splice(0, 1, mutatedName);
+    return ORIGINAL_INJECT_SERVICE.apply(this, args);
   };
 })();
