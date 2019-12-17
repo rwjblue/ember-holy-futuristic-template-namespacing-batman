fde8c9d63da32be74d44572740282d72ca8993a8
Robert Jackson <rjackson@linkedin.com>
2019-12-17 11:32:21 -0500

    Add basic precompile test.

---
+++ node-tests/ast-transform-test.js
@@ -0,0 +1,23 @@
+const { expect } = require('chai');
+const templateCompiler = require('ember-source/dist/ember-template-compiler');
+const Plugin = require('../lib/namespacing-transform').DollarNamespacingTransform
+
+describe('namespacing-transform', function() {
+  function precompile(templateString, _options) {
+    let options = Object.assign({
+      moduleName: 'random-app/template-name.hbs',
+      contents: templateString,
+      plugins: {
+        ast: [Plugin]
+      }
+    }, _options);
+
+    return templateCompiler.precompile(templateString, options);
+  }
+
+  it('can precompile a simple template', function() {
+    let output = precompile('{{foo$bar}}');
+
+    expect(output).to.not.include('$');
+  });
+});
